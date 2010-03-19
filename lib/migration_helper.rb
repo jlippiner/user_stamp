15087488f2a97eebdc7b48a52591051d36457477
Joshua Lippiner <jlippiner@joshua-lippiners-macbook-pro.local>
2010-03-19 11:09:42 -0400

    added postgres send

--- lib/migration_helper.rb
+++ lib/migration_helper.rb
@@ -12,7 +12,6 @@ module UserStamp
     end
 
     module SchemaStatements
-
       # Adds userstamps (creator_id and updater_id) columns to the named table.
       # ===== Examples
       #  add_userstamps(:suppliers)
@@ -21,17 +20,17 @@ module UserStamp
         add_column table_name, :updater_id, :integer
       end
 
-
       # Remove userstamps (creator_id and updater_id) columns from the named table.
       # ===== Examples
-      # remove remove_timestamps(:suppliers)
+      # remove remove_userstamps(:suppliers)
       def remove_userstamps(table_name)
         remove_column table_name, :creator_id
         remove_column table_name, :updater_id
       end
-
     end
+
   end
 end
 
 ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, UserStamp::MigrationHelper)
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, UserStamp::MigrationHelper::SchemaStatements)
